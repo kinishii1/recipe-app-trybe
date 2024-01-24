c305ad3ba6c8fd63f88676d5856be9b5a93bdf9a
Kin Ishii <kinishii97@gmail.com>
2024-01-25 07:37:23 +0900

    implementando share do doneRecipes

--- src/components/DoneRecipes/index.tsx
+++ src/components/DoneRecipes/index.tsx
@@ -1,5 +1,9 @@
+import { useShare } from '../../hooks/useShare';
+import shareIcon from '../../images/shareIcon.svg';
+
 function DoneRecipes() {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes') ?? '') || [];
+  const { handleShareClick, copyStatus } = useShare();
   return (
     <div>
       <div className="done-recipes-btns">
@@ -23,26 +27,32 @@ function DoneRecipes() {
             />
             <p data-testid={ `${index}-horizontal-top-text` }>
               {recipe.type === 'meal'
-                ? `${recipe.area} - ${recipe.category}`
+                ? `${recipe.nationality} - ${recipe.category}`
                 : recipe.alcoholicOrNot}
             </p>
             <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
             <p data-testid={ `${index}-horizontal-done-date` }>
               {recipe.doneDate}
             </p>
-            {
-              recipe.tags.map((tag: any) => (
-                <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
-                  {tag}
-                </span>
-              ))
-            }
+            {recipe.tags.map((tag: any) => (
+              <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
+                {tag}
+              </span>
+            ))}
             <button
               type="button"
-              data-testid={ `${index}-horizontal-share-btn` }
               className="horizontal-share-btn"
+              onClick={ () => handleShareClick(recipe.id, recipe.type === 'drink') }
             >
-              Share
+              {copyStatus !== '' ? (
+                <p>{copyStatus}</p>
+              ) : (
+                <img
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src={ shareIcon }
+                  alt="share"
+                />
+              )}
             </button>
             <button
               type="button"
--- src/hooks/useShare.ts
+++ src/hooks/useShare.ts
@@ -3,9 +3,17 @@ import { useState } from 'react';
 export const useShare = (): any => {
   const [copyStatus, setCopyStatus] = useState<string>('');
 
-  const handleShareClick = async () => {
-    let url = window.location.href;
-    url = url.replace(/\/in-progress$/, '');
+  const handleShareClick = async (id: number = 0, isDrink: boolean = false) => {
+    let url;
+    url = window.location.href;
+    url = url.replace(/\/in-progres$/, '');
+    if (id > 0) {
+      if (isDrink) {
+        url = `${window.location.origin}/drinks/${id}`;
+      } else {
+        url = `${window.location.origin}/meals/${id}`;
+      }
+    }
     try {
       await navigator.clipboard.writeText(url);
       setCopyStatus('Link copied!');
