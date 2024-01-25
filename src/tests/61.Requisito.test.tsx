a241f80405ff31438c521194c20c25ba9fb171ca
Kin Ishii <kinishii97@gmail.com>
2024-01-25 10:41:34 +0900

    100% overall coverage

---
+++ src/tests/61.Requisito.test.tsx
@@ -0,0 +1,40 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { favoriteRecipes } from './mocks/formattedData';
+import FavoriteRecipes from '../components/FavoriteRecipes';
+import ProfilePage from '../pages/ProfilePage';
+
+describe('profile', () => {
+  beforeEach(() => {
+    localStorage.setItem('user', JSON.stringify([{ email: '' }]));
+  });
+  it('test if doneRecipesButton works', async () => {
+    render(
+      <MemoryRouter>
+        <ProfilePage />
+      </MemoryRouter>,
+    );
+    const doneRecipesButton = screen.getByTestId('profile-done-btn');
+
+    await userEvent.click(doneRecipesButton);
+  });
+  it('test if favoriteRecipesButton works', async () => {
+    render(
+      <MemoryRouter>
+        <ProfilePage />
+      </MemoryRouter>,
+    );
+    const favoriteRecipesButton = screen.getByTestId('profile-favorite-btn');
+    await userEvent.click(favoriteRecipesButton);
+  });
+  it('test if logoutButton works', async () => {
+    render(
+      <MemoryRouter>
+        <ProfilePage />
+      </MemoryRouter>,
+    );
+    const logoutButton = screen.getByTestId('profile-logout-btn');
+    await userEvent.click(logoutButton);
+  });
+});
