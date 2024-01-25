132aaa53ce97c52f3d97239d9dbb0a695d9ef44d
Kin Ishii <kinishii97@gmail.com>
2024-01-25 21:45:14 +0900

    terminando refatoração todos componentes iniciar correção erro que apareceu no caminho

---
+++ src/components/Recipes/RecipeCard.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function RecipeCard({ recipe, index }: { recipe: any; index: number }) {
+  const type = recipe.idMeal ? 'meals' : 'drinks';
+  return (
+    <div data-testid={ `${index}-recipe-card` } key={ recipe.idMeal || recipe.idDrink }>
+      <Link to={ `/${type}/${recipe.idMeal || recipe.idDrink}` }>
+        <h1 data-testid={ `${index}-card-name` }>{recipe.strMeal || recipe.strDrink }</h1>
+        <img
+          data-testid={ `${index}-card-img` }
+          src={ recipe.strMealThumb || recipe.strDrinkThumb }
+          alt={ recipe.strMeal || recipe.strDrink }
+        />
+      </Link>
+    </div>
+  );
+}
+
+export default RecipeCard;
---
+++ src/hooks/useSelectedCategory.ts
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+
+type UseSelectedCategoryProps = {
+  isDrink: boolean;
+  recipes: any[];
+  setRecipes: (recipes: any[]) => void;
+};
+
+export const useSelectedCategory = ({
+  isDrink,
+  recipes,
+  setRecipes,
+}: UseSelectedCategoryProps) => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [originalRecipes, setOriginalRecipes] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (selectedCategory === '') return;
+    if (isDrink) {
+      setOriginalRecipes(recipes);
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${selectedCategory}`,
+      )
+        .then((response) => response.json())
+        .then((data) => data.drinks.slice(0, 12))
+        .then((data) => setRecipes(data));
+    } else {
+      setOriginalRecipes(recipes);
+      fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`,
+      )
+        .then((response) => response.json())
+        .then((data) => data.meals.slice(0, 12))
+        .then((data) => setRecipes(data));
+    }
+  }, [selectedCategory]);
+
+  const handleClick = (event: any) => {
+    const category = event.target.value;
+    setSelectedCategory((prevCategory) => (
+      prevCategory === category ? handleClearFilter() : category
+    ));
+  };
+
+  const handleClearFilter = () => {
+    setSelectedCategory('');
+    setRecipes(originalRecipes);
+  };
+};
