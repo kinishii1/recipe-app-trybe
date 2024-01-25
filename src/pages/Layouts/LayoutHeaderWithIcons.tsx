ae3708103904e1a9591978200dd1154fcd51636d
Kin Ishii <kinishii97@gmail.com>
2024-01-25 22:19:17 +0900

    ajustando layout headers

--- src/pages/Layouts/LayoutHeaderWithIcons.tsx
+++ src/pages/Layouts/LayoutHeaderWithIcons.tsx
@@ -2,9 +2,11 @@ import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function LayoutHeaderWithtIcons() {
+  const actualPage = window.location.pathname;
+  const title = actualPage === '/meals' ? 'Meals' : 'Drinks';
   return (
     <div>
-      <Header title="Done Recipes" withSearchIcons />
+      <Header title={ title } withSearchIcons />
       <Outlet />
     </div>
   );
--- src/pages/Layouts/LayoutHeaderWithoutIcons.tsx
+++ src/pages/Layouts/LayoutHeaderWithoutIcons.tsx
@@ -2,9 +2,21 @@ import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header';
 
 function LayoutHeaderWithoutIcons() {
+  const determineTitle = () => {
+    const actualPage = window.location.pathname;
+    switch (actualPage) {
+      case '/done-recipes':
+        return 'Done Recipes';
+      case '/favorite-recipes':
+        return 'Favorite Recipes';
+      default:
+        return 'Profile';
+    }
+  };
+  const title = determineTitle();
   return (
     <div>
-      <Header title="Done Recipes" withSearchIcons={ false } />
+      <Header title={ title } withSearchIcons={ false } />
       <Outlet />
     </div>
   );
